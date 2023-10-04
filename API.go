74ac8c495360bbff8e8b3bace3abf99fd97d8940
nathanplenty <nathan.roskamp@plentysystems.com>
2023-10-04 13:14:44 +0200

    Added web based login

--- API.go
+++ API.go
@@ -2,8 +2,61 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"net/http"
+)
+
+var (
+	users = map[string]string{
+		"user1": "password1",
+	}
 )
 
 func main() {
-	fmt.Print("TEST")
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/login", loginHandler)
+	http.HandleFunc("/dashboard", dashboardHandler)
+	fmt.Println("Server is running on :8080")
+	http.ListenAndServe(":8080", nil)
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := `
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Login</title>
+    </head>
+    <body>
+        <h1>Login Page</h1>
+        <form method="POST" action="/login">
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username" required><br><br>
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required><br><br>
+            <input type="submit" value="Login">
+        </form>
+    </body>
+    </html>
+    `
+	t, _ := template.New("index").Parse(tmpl)
+	t.Execute(w, nil)
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+	if storedPassword, ok := users[username]; ok && storedPassword == password {
+		http.Redirect(w, r, "/dashboard", http.StatusSeeOther)
+		return
+	}
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+func dashboardHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Welcome to the Dashboard!")
 }
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 04.10.2023 - Create Workspace
+04.10.2023 - Added simple web based login function

f3764a0bfb328ccfca5e7367d8eebb8bbfe5799d
nathanplenty <nathan.roskamp@plentysystems.com>
2023-10-04 13:46:40 +0200

    Added create user

--- API.go
+++ API.go
@@ -1,23 +1,51 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"html/template"
 	"net/http"
+
+	"github.com/gorilla/mux"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 var (
-	users = map[string]string{
-		"user1": "password1",
-	}
+	db *sql.DB
 )
 
 func main() {
-	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/login", loginHandler)
-	http.HandleFunc("/dashboard", dashboardHandler)
+	var err error
+	db, err = sql.Open("sqlite3", "database.db")
+	if err != nil {
+		fmt.Println("Error opening database:", err)
+		return
+	}
+	defer db.Close()
+
+	createTable()
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", indexHandler).Methods("GET")
+	r.HandleFunc("/login", loginHandler).Methods("POST")
+	r.HandleFunc("/dashboard", dashboardHandler).Methods("GET")
+	r.HandleFunc("/adduser", addUserHandler).Methods("POST")
+
+	http.Handle("/", r)
+
 	fmt.Println("Server is running on :8080")
 	http.ListenAndServe(":8080", nil)
+}
+
+func createTable() {
+	_, err := db.Exec(`CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT UNIQUE NOT NULL,
+        password TEXT NOT NULL
+    )`)
+	if err != nil {
+		fmt.Println("Error creating table:", err)
+	}
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
@@ -36,6 +64,16 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
             <input type="password" id="password" name="password" required><br><br>
             <input type="submit" value="Login">
         </form>
+
+        <!-- Formular zum Hinzufügen eines Benutzers -->
+        <h2>Add User</h2>
+        <form method="POST" action="/adduser">
+            <label for="new_username">New Username:</label>
+            <input type="text" id="new_username" name="new_username" required><br><br>
+            <label for="new_password">New Password:</label>
+            <input type="password" id="new_password" name="new_password" required><br><br>
+            <input type="submit" value="Add User">
+        </form>
     </body>
     </html>
     `
@@ -43,17 +81,48 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	t.Execute(w, nil)
 }
 
+func addUserHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
+	newUsername := r.FormValue("new_username")
+	newPassword := r.FormValue("new_password")
+
+	if err := addUser(newUsername, newPassword); err != nil {
+		fmt.Println("Error adding user:", err)
+	}
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+func addUser(username, password string) error {
+	_, err := db.Exec("INSERT INTO users (username, password) VALUES (?, ?)", username, password)
+	return err
+}
+
 func loginHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		http.Redirect(w, r, "/", http.StatusSeeOther)
 		return
 	}
+
 	username := r.FormValue("username")
 	password := r.FormValue("password")
-	if storedPassword, ok := users[username]; ok && storedPassword == password {
+
+	var storedPassword string
+	err := db.QueryRow("SELECT password FROM users WHERE username = ?", username).Scan(&storedPassword)
+	if err != nil {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
+	if storedPassword == password {
 		http.Redirect(w, r, "/dashboard", http.StatusSeeOther)
 		return
 	}
+
 	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 04.10.2023 - Create Workspace
 04.10.2023 - Added simple web based login function
+04.10.2023 - Added create user and save information in database
